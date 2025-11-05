bb2fedb96f303bb40292663694f501c49d96c1b2
Mahes <mahesavpm123@gmail.com>
2025-11-05 15:49:21 +0700

    feat: add login style and logic with dummy

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,58 @@
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import React, {useState} from "react";
 
 const Login = () => {
     const navigate = useNavigate();
+    const [name, setName] = useState("");
 
-    const handleLogin = () => {
-        // ganti logic Firebase Auth
+    const handleLogin = (e) => {
+        e.preventDefault();
 
+        // Simulasi login sederhana
+        if (name.trim() === "") {
+            alert("Masukkan nama terlebih dahulu!");
+            return;
+        }
 
         localStorage.setItem("auth", "true");
-        navigate("/Home");
+        localStorage.setItem("username", name);
+
+        navigate("/home");
     };
 
+
+
+
     return (
-        <div className="flex flex-col items-center justify-center h-screen gap-4">
-            <h1 className="text-2xl font-semibold">Login</h1>
-            <button
-                onClick={handleLogin}
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            >
-                Simulasi Login
-            </button>
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="">
+                <form
+                    onSubmit={handleLogin}
+                    className="bg-white p-8 rounded-2xl shadow-md w-80"
+                >
+                    <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+
+                    <input
+                        type="text"
+                        placeholder="Masukan nama"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="w-full p-2 bg-background border border-[#d9d9d9] rounded-md mb-4"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Masukan Password"
+                        className="w-full p-2 bg-background border border-[#d9d9d9] rounded-md mb-4"
+                    />
+
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition"
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
