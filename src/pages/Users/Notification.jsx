959b378a32c2d0371dddbda98940401408a66191
Mahes <mahesavpm123@gmail.com>
2025-12-18 17:19:44 +0700

    fix denda

---
+++ src/pages/Users/Notification.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notification = () => {
+    return (
+        <div>
+            <Text size="lg" className="mb-4">Ini Notification page</Text>
+        </div>
+    );
+};
+
+export default Notification;
