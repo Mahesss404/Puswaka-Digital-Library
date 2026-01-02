c97afd01529dfe89d3badebb07ba68845f2f9724
Mahes <mahesavpm123@gmail.com>
2026-01-02 13:39:51 +0700

    scroll restoration to top on route changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useLayoutEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
 /**
  * Layout component that wraps Header and Footer around child pages
  * This eliminates the need to import Header/Footer on each individual page
+ * Includes scroll restoration to top on route changes
  */
 const Layout = () => {
+    const { pathname } = useLocation();
+
+    // Scroll to top on route change - useLayoutEffect prevents flicker
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
     return (
         <div className="min-h-screen flex flex-col">
             <Header />
